21aa1197c9ae19226f943896ad8d70b786ccfc4e
Ali <ali.subhani@tintash.com>
2024-10-06 14:59:47 +0300

    Add code to make the entire project card clickable

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import {  EyeIcon } from "@heroicons/react/24/outline";
+import { EyeIcon } from "@heroicons/react/24/outline";
 
-const ProjectCard = ({ imgUrl, title, description,  previewUrl }) => {
+const ProjectCard = ({ imgUrl, title, description, previewUrl }) => {
   return (
-    <div>
+    <a href={previewUrl} target="_blank">
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          
           <a
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-          target="_blank"
+            target="_blank"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </a>
@@ -23,7 +22,7 @@ const ProjectCard = ({ imgUrl, title, description,  previewUrl }) => {
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
-    </div>
+    </a>
   );
 };
 
